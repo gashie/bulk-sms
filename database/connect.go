fc5dea0fed191cb058874b700a2a89d29e5a992c
gashie <richardwilliam60@gmail.com>
2022-12-06 08:39:09 +0000

    first commit

---
+++ database/connect.go
@@ -0,0 +1,22 @@
+package database
+
+import (
+	"bulk-sms/models"
+
+	"gorm.io/driver/mysql"
+	"gorm.io/gorm"
+)
+
+var DB *gorm.DB
+
+func Connect() {
+	database, err := gorm.Open(mysql.Open("mygo:mygo@tcp(192.168.64.2:3306)/go_sms?charset=utf8mb4&parseTime=True&loc=Local"), &gorm.Config{})
+
+	if err != nil {
+		panic("Could not connect to the database")
+	}
+
+	DB = database
+	database.Migrator().DropTable(&models.Group{})
+	database.AutoMigrate(&models.Group{})
+}
---
+++ models/Group.go
@@ -0,0 +1,9 @@
+package models
+
+type Group struct {
+	Id uint `json:"id" gorm:"primaryKey"`
+
+	Title        string `json:"title" gorm:"unique"`
+	Description string `json:"description"`
+	Base
+}
